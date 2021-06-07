b98f0a1484d22355a509cc632839dba20f2d5fe7
albertoparras-dev <66566819+albertoparras-dev@users.noreply.github.com>
2021-06-07 04:13:49 +0200

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,28 +1,31 @@
-module.exports = {
-    name: 'prefix',
-    description: 'Changes the bot prefix',
-    async execute(Discord, client, message, args){
-        const db = require('quick.db');
-        var guildInfo = new db.table('guildInfo');
-        var split = message.content.split(' ');
-        customPrefix = split[1];
+module.exports = (Discord, client, message) => {
+    const db = require('quick.db');
+    var guildInfo = new db.table('guildInfo');
+    let examplePrefix = '!';
+    let serverPrefix = guildInfo.get(`${message.guild.id}.prefix`)
+    if (serverPrefix === null) serverPrefix = process.env.DEFAULTPREFIX;
+    if (serverPrefix === '!') examplePrefix = '-';
 
-        if (!message.member.hasPermission("ADMINISTRATOR")) {
-            message.channel.send("You don't have enough permissions to use this command.")
-            return;
-        }
-        
-        if (!args[0]) {
-            message.channel.send('This command needs args, mention me for help');
-            return;
-        }
+    const help =  new Discord.MessageEmbed()
+    .setColor('white')
+    .setAuthor('MMR Bot - Help', client.user.displayAvatarURL())
+    .setThumbnail(client.user.displayAvatarURL())
+    .setDescription("Hey! Here you have my commands. I'll remind you that I'm in beta, tons of features are coming.")
+    .addFields(
+        { name: `${serverPrefix}mmr region user`, value: `Gives the MMR of a user. Example: ${serverPrefix}mmr euw pr1meé` },
+        { name: `${serverPrefix}bot`, value: 'Gives info of the bot (Links, author and repo)' },
+        { name: `${serverPrefix}invite`, value: 'Gives the invite link of the bot' },
+        { name: `${serverPrefix}prefix`, value: `Changes the prefix of the bot for this server (You need ADMINISTRATOR permissions). Example: ${serverPrefix}prefix ${examplePrefix}`}
+    )
+    .setFooter('Thanks for using MMR Bot', 'https://raw.githubusercontent.com/albertoparras-dev/MMR_Bot/main/img/Hearth.png');
 
-        if (args[1]) {
-            message.channel.send("The prefix can't have spaces.")
-            return;
-        }
+    if (message.mentions.has(client.user.id)) message.channel.send(help);
+    if (message.content.includes("@here") || message.content.includes("@everyone")) return false;
 
-        guildInfo.set(`${message.guild.id}`, { prefix: `${customPrefix}` });
-        message.channel.send(`The prefix has been changed succesfully to: ${customPrefix}`);
-    }
+    if (!message.content.startsWith(serverPrefix) || message.author.bot) return;
+    const args = message.content.slice(serverPrefix.length).split(/ +/);
+    const cmd = args.shift().toLowerCase();
+    const command = client.commands.get(cmd);
+
+    if(command) command.execute(Discord, client, message, args);
 }
